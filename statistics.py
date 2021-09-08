6849cea8d478cf57eadd68fc9d305348f38c22e0
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-09-08 19:58:09 -0300

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,15 +4,14 @@ def mean(X: list) -> float:
     return sum(X) / len(X)
 
 def mode(X: list) -> float:
-    return max(set(X), key=X.count)
+    return max(set(X), key=list(X).count)
 
 def median(X: list) -> float:
     X_sorted = sorted(X)
     n = len(X)
     if n % 2 != 0:
         return X_sorted[(n - 1) // 2]
-    else:
-        return (X_sorted[(n - 1) // 2] + X_sorted[((n - 1) // 2) + 1]) / 2
+    return (X_sorted[(n - 1) // 2] + X_sorted[((n - 1) // 2) + 1]) / 2
 
 def variance(X: list) -> float:
     return sum([(x - mean(X))**2 for x in X]) / (len(X) - 1)
