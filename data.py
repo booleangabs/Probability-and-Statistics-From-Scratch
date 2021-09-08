944d35269c6af5efe0d7b3b6638963f21b17734d
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-09-08 11:54:10 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,24 @@
 class DataFrame:
     pass
 
-class TimeSeries:
+def parseCsv(path: str) -> DataFrame:
+    file = open(path)
+    data = {}
+    headers = [i for i in file.readline().strip('\n').split(',')]
+    for i in headers:
+        data[i] = []
+    while True:
+        line = file.readline()
+        if line == '':
+            break
+        line = parseLine(line)
+        for i, h in enumerate(headers):
+            data[h].append(line[i])
+            
+    return DataFrame(data)
+        
+def parseLine(line: str) -> list:
     pass
+        
+        
+        
